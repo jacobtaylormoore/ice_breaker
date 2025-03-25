6f6f74300cb802abf4dfabc955ceea24d8871aa1
Jake <jacobtaylormoore@gmail.com>
2025-03-25 14:59:05 -0500

    video one

---
+++ ice_breaker.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+
+from third_parties.linkedin import scrape_linkedin_profile
+
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("hello world")
+
+    summary_template = """"
+    given the LinkedIn information {information} about a person from I want you to create:
+    1. a short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOllama(model="mistral")
+
+    chain = summary_prompt_template | llm | StrOutputParser()
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/gabag26/"
+    )
+    res = chain.invoke(input={"information": linkedin_data})
+
+    print(res)
