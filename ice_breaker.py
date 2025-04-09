dc89812c634dfb6f3d14b69a0bd2cdc239b08d95
Jake <jacobtaylormoore@gmail.com>
2025-04-09 10:22:53 -0500

    updates through part 4

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,42 +1,54 @@
 import os
+from typing import Tuple
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
+from langchain_anthropic import ChatAnthropic
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser, Summary
+### Change for mock 
+MOCK=False
 
-
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary,str]:
     linkedin_url = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url, mock=MOCK)
     summary_template = """"
     given the LinkedIn information {information} about a person I want you to create:
     1. a short summary
     2. two interesting facts about them
+    
+    \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+            }
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     # llm = ChatOllama(model="mistral")
     
-    chain = summary_prompt_template | llm | StrOutputParser()
+    # chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information": linkedin_data})
+    res:Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    photo = linkedin_data.get("photoUrl")
+
+    return res, photo
 
 
 
 if __name__ == "__main__":
     load_dotenv()
     print("Ice Breaker Enter")
-    ice_break_with(name="Bo French Tarrant County Republican Party")
+    ice_break_with(name="Shane Barker")
 
  
 
